2dd1cc8c15a0278ef3670ce4cf3e1df5796a37ce
Amanda Souza <amanda_souza@live.com>
2022-06-01 08:56:48 -0300

    first commit

---
+++ exercícios.js
@@ -0,0 +1,36 @@
+/* 
+1. Armazene seu nome em uma variável, depois faça a concatenação com a string  'Olá'  para escrever no console 'Olá SEUNOME'
+
+2. Crie um programa que calcule a média dos valores:
+10.5
+33.96
+20.2
+8.4 
+
+3. Armazene os nomes de 5 frutas em um array, e escreva no console a fruta que esteja no índice 3.
+
+4. Crie um programa que diga qual o comprimento das strings:
+'javaScript'
+'desenvolvimento'
+'1+1'
+
+5. Crie uma função para cada uma das operações básicas da matemática (adição, subtração, divisão e multiplicação). As funções devem receber dois parâmetros numéricos e retornar o resultado da operação.
+
+6. Crie um programa que identifique se um número é par ou ímpar
+
+7. Crie um programa que identifique se uma pessoa é maior ou menor de idade
+
+8. Crie uma função que receba 2 números inteiros como parâmetros, e retorne o maior valor entre eles.
+
+9. Crie um programa que armazene seu nome em uma constante, e escreva ele de trás para frente.
+
+10. Crie uma função que receba um valor numérico como parâmetro, e faça as seguintes comparações:
+Se o valor de entrada for divisível por 3, então escreva "Fizz" no console.
+Se o valor de entrada for divisível por 5, então escreva "Buzz" no console.
+Se o valor de entrada for divisível por 3 e 5, então escreva "FizzBuzz" no console.
+Se o valor de entrada não for divisível por 3 ou 5, escreva apenas o número no console.
+
+Dica: Qualquer número divisível por 3 ou 5, tem o resto da divisão igual a zero.
+
+
+*/
